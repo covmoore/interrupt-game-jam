aee171dd30aa0aee85928c364ca284c0b583a2f6
Nicholas Facciola <nicholasfacciola@gmail.com>
2025-01-18 15:54:56 -0700

    brehg fix to bullet

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -6,6 +6,10 @@ extends RigidBody3D
 func _ready():
 	await(get_tree().create_timer(lifespan)).timeout
 	queue_free()
+
+func initialize_bullet(target_position: Vector3, bullet_speed: float):
+	look_at_from_position(global_transform.origin, target_position, Vector3.UP)
+	linear_velocity = global_transform.basis.z * bullet_speed
 	
 func _process(delta: float) -> void:
 	global_transform.origin += global_transform.basis.z * -1 * speed * delta
--- scripts/gun.gd
+++ scripts/gun.gd
@@ -9,21 +9,8 @@ func shoot():
 	if can_shoot:
 		can_shoot = false
 		var bullet = bullet_scene.instantiate()
-		
-		# Set the initial position before adding to the tree
-		bullet.global_transform.origin = $Muzzle.global_transform.origin
-		
-		# Add the bullet to the scene tree first
 		get_tree().root.add_child(bullet)
-		
-		# Look at the target and set velocity after it's in the tree
-		bullet.look_at_from_position(
-			bullet.global_transform.origin,
-			$Muzzle.global_transform.origin + $Muzzle.global_transform.basis.z,
-			Vector3.UP
-		)
-		bullet.linear_velocity = bullet.global_transform.basis.z * shoot_speed
-		
-		# Handle fire rate cooldown
+		bullet.global_transform.origin = $Muzzle.global_transform.origin
+		bullet.call_deferred("initialize_bullet", $Muzzle.global_transform.origin + $Muzzle.global_transform.basis.z, shoot_speed)
 		await get_tree().create_timer(fire_rate).timeout
 		can_shoot = true
