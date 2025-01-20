1aee66467901181dbae075f8f7f52b1716fd41ba
Nicholas Facciola <nicholasfacciola@gmail.com>
2025-01-19 22:46:36 -0700

    gun works

--- scripts/models/gun.gd
+++ scripts/models/gun.gd
@@ -1,16 +1,17 @@
-extends Node
+extends Node3D
 
 @export var bullet_scene: PackedScene
-@export var shoot_speed: float = 50.0
-@export var fire_rate: float = 0.2
+@export var muzzle_speed: float = 50.0
+@export var fire_rate:float = 0.2
 var can_shoot = true
 
 func shoot():
 	if can_shoot:
 		can_shoot = false
-		var bullet = bullet_scene.instantiate()
-		get_tree().root.add_child(bullet)
-		bullet.global_transform.origin = $Muzzle.global_transform.origin
-		bullet.call_deferred("initialize_bullet", $Muzzle.global_transform.origin + $Muzzle.global_transform.basis.z, shoot_speed)
+		var new_bullet = bullet_scene.instantiate()
+		new_bullet.global_transform = $Muzzle.global_transform
+		#new_bullet.speed = muzzle_speed
+		var scene_root = get_tree().get_root().get_children()[0]
+		scene_root.add_child(new_bullet)
 		await get_tree().create_timer(fire_rate).timeout
 		can_shoot = true
