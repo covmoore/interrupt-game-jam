c9fcdbdcb2e4cabda8f153bf3eb4d6ba45dc9eed
Nicholas Facciola <nicholasfacciola@gmail.com>
2025-01-23 14:47:08 -0700

    edited enemy

--- scripts/models/enemy.gd
+++ scripts/models/enemy.gd
@@ -5,6 +5,8 @@ class_name Enemy extends CharacterBody3D
 @export var movement_speed : float = 2.0
 @export var health: float = 0.0
 
+@export var attack_range: float = 5.0
+
 var movement_target_position : Vector3 = Vector3.ZERO
 var path = []
 
@@ -13,7 +15,15 @@ func _ready():
 	nav_agent.target_desired_distance = 0.5
 	
 func set_movement_target(movement_target: Vector3):
-	nav_agent.set_target_position(movement_target)
+	#Instead of going straight to the player pick a random offset
+	#to avoid bunching up
+	var offset_distance = 2.0
+	var random_angle = randf_range(0, TAU)
+	var offset_x = cos(random_angle) * offset_distance
+	var offset_z = sin(random_angle) * offset_distance
+	
+	var target_pos = player.global_transform.origin + Vector3(offset_x, 0, offset_z)
+	nav_agent.set_target_position(target_pos)
 	
 func actor_setup():
 	await get_tree().physics_frame
@@ -22,14 +32,27 @@ func actor_setup():
 	
 func _physics_process(delta):
 	actor_setup()
-	if nav_agent.is_navigation_finished():
-		return
+	var distance_to_player = global_position.distance_to(player.global_transform.origin)
 	
-	var current_agent_position = global_position
-	var next_path_position = nav_agent.get_next_path_position()
-	
-	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	#If close enough to attack
+	if distance_to_player <= attack_range:
+		#Stop moving
+		velocity = Vector3.ZERO
+		#Face the player
+		look_at(player.global_transform.origin, Vector3.UP)
+		#call you shooting logic
+		shoot_at_player()
+	else:
+		#otherwise, keep using the nav agent
+		if not nav_agent.is_navigation_finished():
+			var next_path_position = nav_agent.get_next_path_position()
+			velocity = global_position.direction_to(next_path_position) * movement_speed
+		else:
+			velocity = Vector3.ZERO
 	move_and_slide()
+	
+func shoot_at_player():
+	print("Pew pew! Suck my ass!!!!!")
 
 func take_damage(shot_by: CharacterBody3D, dmg: float):
 	health -= dmg
