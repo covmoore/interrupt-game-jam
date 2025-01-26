b86ca2f6b1cf7bcbca05dc1223aedb6d00d65ca6
Paul Thillier <80140723+PaulThillier@users.noreply.github.com>
2025-01-26 00:27:44 -0700

    Added new painting and changed some folders,. need to update calls with new folder paths

---
+++ maps/player_ui.gd
@@ -0,0 +1,38 @@
+extends Control
+
+@onready var cam: Camera3D = $"../CameraPivot/Camera3D"
+@onready var canvas: CanvasLayer = $CanvasLayer
+@onready var hud = $CanvasLayer/MarginContainer/hud
+@onready var interactMenu = $CanvasLayer/MarginContainer/interactMenu
+signal cancel_interaction
+var panels = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	panels = [hud, interactMenu]
+	show_panels([hud])
+	#interactMenu.visible = false
+	var interactables = get_tree().get_nodes_in_group("interactable")
+	for interactable in interactables:
+		interactable.connect("on_object_interacted", _on_object_interacted)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_object_interacted() -> void:
+	show_panels([interactMenu])
+	#interactMenu.visible = true
+
+func _on_button_button_down() -> void:
+	show_panels([hud])
+	emit_signal("cancel_interaction")
+
+func show_panels(curPanel: Array[Control]):
+	for panel in panels:
+		if curPanel.has(panel):
+			panel.show()
+		else:
+			panel.hide()
