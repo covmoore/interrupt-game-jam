2a3e9e98459fe7df0f54c2a61a74e74f690dd077
Paul Thillier <80140723+PaulThillier@users.noreply.github.com>
2025-01-26 00:27:44 -0700

    Added new painting and changed some folders,. need to update calls with new folder paths

---
+++ maps/panel.gd
@@ -0,0 +1,20 @@
+extends Control
+class_name UIPanel
+
+@export var hidesUI = true
+@export var pausesGame = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func show():
+	super()
+
+func hide():
+	super()
