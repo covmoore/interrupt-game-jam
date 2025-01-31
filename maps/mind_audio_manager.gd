c1be9123ae5e4e92fc3d2e5a09178f30ac9452b4
covmoore <covmoore5@gmail.com>
2025-01-30 22:28:58 -0700

    adding mind music and fixing UI changes

--- maps/mind_audio_manager.gd
+++ maps/mind_audio_manager.gd
@@ -1,24 +1,31 @@
 extends Node3D
 class_name MindAudioManager
 
-@onready var mind_song = $mind_song
+@onready var mind_song_p1 = $mindSongP1
+@onready var mind_song_p2 = $mindSongP2
 @onready var wind_sound = $wind
 
 @export var camera: Node3D = null
 
 func _ready() -> void:
-	mind_song.stream = preload("res://sounds/songs/ascension_into_madness.wav")
+	mind_song_p1.stream = preload("res://sounds/songs/ascension_into_madness_pt1.mp3")
+	mind_song_p2.stream = preload("res://sounds/songs/ascension_into_madness_pt2.mp3")
 	wind_sound.stream = preload("res://sounds/wind.mp3")
-	mind_song.connect("finished", _on_mind_song_finished)
+	mind_song_p1.connect("finished", _on_mind_song_p1_finished)
+	mind_song_p2.connect("finished", _on_mind_song_p2_finished)
 	wind_sound.connect("finished", _on_wind_sound_finished)
 	play_mind_soundtrack()
 
 func play_mind_soundtrack():
 	#check if song is already playing and only play if not playing
-	if not mind_song.is_playing():
-		mind_song.play()
+	if not mind_song_p1.is_playing():
+		mind_song_p1.play()
 
-func _on_mind_song_finished():
+func _on_mind_song_p1_finished():
+	# Play the wind sound and set it to loop
+	mind_song_p2.play()
+
+func _on_mind_song_p2_finished():
 	# Play the wind sound and set it to loop
 	wind_sound.play()
 	
---
+++ prefabs/hud.gd
@@ -0,0 +1,27 @@
+extends UIPanel
+class_name HUD
+
+@onready var timer = $Timer
+@onready var uiManager: UIManager = $"../../.."
+var gameManager:GameManager = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	gameManager = uiManager.gameManager
+
+func _process(delta: float) -> void:
+	if uiManager.ui_state == uiManager.UIState.IDLE and gameManager != null:
+		var time_elapsed: float = gameManager.timer.time_left
+		var minutes: int = time_elapsed / 60
+		var seconds: int = fmod(time_elapsed, 60)
+		timer.text = "%02d:%02d" % [minutes,seconds]
+
+func show():
+	super()
+	if uiManager.ui_state == uiManager.UIState.IDLE:
+		timer.show()
+	else:
+		timer.hide()
+
+func hide():
+	super()
